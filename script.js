31c043fb765157bfcc1624c93166241618e19b7e
ChetanPrince <saini.rajvir2@gmail.com>
2024-07-11 10:17:28 +0200

    basic crud functioality added

---
+++ script.js
@@ -0,0 +1,53 @@
+let selectedRow = null;
+function onSubmitForm(e){
+    e.preventDefault();
+    if(selectedRow === null){
+        addBookmark();
+    }
+    else{
+        updateForm();
+    }
+    resetForm();
+
+};
+
+
+
+const addBtn = document.getElementById("add-btn");
+const deleteBtn = document.getElementById("delete-btn");
+
+
+function addBookmark(){
+    const wrapper = document.getElementById("bookmark-table").querySelector('tbody');
+    const inputUrl = document.getElementById("book-url").value;
+   
+    const tr =document.createElement("tr");
+    tr.innerHTML = `<td>${inputUrl}</td>
+    <td onClick="edit(this)"><button>Edit</button></td>
+    <td onClick="deleteItem(this)"><button>Delete Bookmark</button></td>`;
+    wrapper.appendChild(tr);
+    
+}
+
+function edit(td){
+  selectedRow = td.parentElement;
+    document.getElementById("book-url").value = selectedRow.cells[0].innerHTML;
+}
+function updateForm(){
+    selectedRow.cells[0].innerHTML = document.getElementById("book-url").value;
+    selectedRow = null;
+}
+function deleteItem(td){
+    if(confirm("Are you sure you want to delete this record?")){
+       row = td.parentElement.parentElement;
+       row.remove();
+       resetForm();
+    } 
+}
+
+function resetForm(){
+    document.getElementById("book-url").value = "";
+}
+
+
+addBtn.addEventListener("click", onSubmitForm);
